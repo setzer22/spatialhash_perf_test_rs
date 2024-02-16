da6f975f8012e15edec24b04f3314435c9ba65c0
Jan Haller <bromeon@gmail.com>
2024-02-16 19:44:10 +0100

    Fancy benchmark suite

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,34 @@
 pub mod spatial_hash;
+
 use spatial_hash::*;
+use std::time::Duration;
 
 use glam::*;
 
-use rand::Rng;
+use rand::rngs::StdRng;
+use rand::{Rng, SeedableRng};
+
+const WARMUP_ITERS: usize = 0;
+const MEASURE_ITERS: usize = 700;
 
 fn main() {
     let mut spatial = SpatialHash::new();
 
-    let mut avg_add = 0.0;
-    let mut avg_query = 0.0;
-    let mut avg_total = 0.0;
-    let mut iterations = 0.0;
+    let mut min_add = None;
+    let mut min_query = None;
+
+    let mut median_add = Vec::new();
+    let mut median_query = Vec::new();
+
+    let mut iterations = 0;
+    let seed = 12345;
+    let mut rng = StdRng::seed_from_u64(seed);
+
+    // Note: since rng calls C, might be non-portable, and you might want to adjust results on your machine.
+    let expected_results = [52696, 55359, 53132, 52800, 54877];
 
     loop {
         let time = std::time::Instant::now();
-        let mut rng = rand::thread_rng();
 
         let mk_circle = |pos: Vec2| {
             Shape::Circle(CircleShape {
@@ -81,19 +94,59 @@ fn main() {
             count += result.count();
         }
 
-        let elapsed_query = time.elapsed();
+        iterations += 1usize;
+        if iterations >= WARMUP_ITERS {
+            let elapsed_query = time.elapsed();
+            //
+            // avg_add += elapsed_add.as_micros() as f64;
+            // avg_query += elapsed_query.as_micros() as f64;
+            // avg_total += time.elapsed().as_micros() as f64;
 
-        iterations += 1.0;
-        avg_add += elapsed_add.as_micros() as f64;
-        avg_query += elapsed_query.as_micros() as f64;
-        avg_total += time.elapsed().as_micros() as f64;
+            min_add = min_add
+                .map(|x| Duration::min(x, elapsed_add))
+                .or(Some(elapsed_add));
+            min_query = min_query
+                .map(|x| Duration::min(x, elapsed_query))
+                .or(Some(elapsed_query));
 
-        println!(
-            "C: {count} A: {:.2}us ... Q: {:.2}us .. T: {:.2}us",
-            avg_add / iterations,
-            (avg_query - avg_add) / iterations,
-            avg_total / iterations
-        );
+            // Compute median
+            median_add.push(elapsed_add);
+            median_query.push(elapsed_query);
+
+            // End
+            if iterations >= MEASURE_ITERS + 5 {
+                break;
+            }
+
+            // Last iteration
+            if iterations == MEASURE_ITERS + 4 {
+                median_add.sort();
+                median_query.sort();
+
+                println!(
+                    "Min:      A: {:.2}us    Q: {:.2}us",
+                    min_add.unwrap().as_micros() as f64,
+                    min_query.unwrap().as_micros() as f64,
+                );
+
+                println!(
+                    "Median:   A: {:.2}us    Q: {:.2}us",
+                    median_add[MEASURE_ITERS / 2].as_micros() as f64,
+                    median_query[MEASURE_ITERS / 2].as_micros() as f64,
+                );
+            }
+
+            if iterations >= MEASURE_ITERS {
+                // Check deterministic results, to avoid buge
+                assert_eq!(
+                    count,
+                    expected_results[iterations - MEASURE_ITERS],
+                    "BLIN! iz buge (or other RNG)"
+                );
+
+                // println!("Results:   C: {count}");
+            }
+        }
 
         spatial.clear();
     }
